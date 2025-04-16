1f64958a335e56c412933adcec4d289049399ddc
hastiatarod <h.atarod.h@gmail.com>
2025-04-16 21:36:15 +0330

    handle input search engine

--- src/Dictionary/dictionary.jsx
+++ src/Dictionary/dictionary.jsx
@@ -1,10 +1,22 @@
-import React from "react"
+import React, { useState } from "react"
 import './dictionary.css'
 export default function Dictionary() {
+    const [keyWord, setKeyWord] = useState("");
+
+    function search(event) {
+        event.preventDefault();
+        console.log(keyWord);
+
+    }
+
+    function updateTheKeyWord(event) {
+        setKeyWord(event.target.value);
+    }
+
     return (
         <div>
-            <form action="">
-                <input type="search" placeholder="Search for a word..." />
+            <form action="" onSubmit={search}>
+                <input type="search" placeholder="Search for a word..." onChange={updateTheKeyWord} />
                 <button>🔍</button>
             </form>
         </div>
