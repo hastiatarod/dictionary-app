34b7c6e68e95b80fce6a81ceca3735f463727868
hastiatarod <h.atarod.h@gmail.com>
2025-04-19 23:50:33 +0330

    axios added

--- src/Dictionary/dictionary.jsx
+++ src/Dictionary/dictionary.jsx
@@ -1,11 +1,19 @@
 import React, { useState } from "react"
+import axios from "axios";
 import './dictionary.css'
 export default function Dictionary() {
     const [keyWord, setKeyWord] = useState("");
 
+    function handleResponse(response) {
+        console.log(response);
+    }
+
     function search(event) {
         event.preventDefault();
         console.log(keyWord);
+        let key = 'tb9bafb403447563b62afb93o4d8c0fd'
+        let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyWord}&key=${key}`;
+        axios.get(apiUrl).then(handleResponse);
 
     }
 
